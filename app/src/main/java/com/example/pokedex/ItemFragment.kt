e328f6b88bc13e663e30c1aac183a62ca6399505
Lucas Lopes <lopes.lucas128@gmail.com>
2021-11-03 19:56:49 -0300

    Atualização Api

--- app/src/main/java/com/example/pokedex/ItemFragment.kt
+++ app/src/main/java/com/example/pokedex/ItemFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [InfoFragment.newInstance] factory method to
+ * Use the [ItemFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class InfoFragment : Fragment() {
+class ItemFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        return inflater.inflate(R.layout.fragment_item, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class InfoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoFragment.
+         * @return A new instance of fragment ItemFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            InfoFragment().apply {
+            ItemFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
